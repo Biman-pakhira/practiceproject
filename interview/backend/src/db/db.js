386361f1290b2a906071e7ad889af0ff21c36f63
Biman Pakhira <bimanpakhira2@gmail.com>
2026-05-16 12:24:40 +0530

    deleted

--- interview/backend/src/db/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const connnetDB = async()=>{
-    await mongoose.connect("mongodb+srv://Guddu:YaOcpRhFgzEtxSXU@guddu.pjthp5x.mongodb.net/interview");
-    console.log("database Connected successfully");
-    
-}
-
-module.exports=connnetDB;
