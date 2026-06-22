c38c68938482eeb337344d91311ed559a1787f72
Biman Pakhira <bimanpakhira2@gmail.com>
2026-06-22 23:24:50 +0530

    c

--- interview/toDo/src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import newSlice from './features/featureSlice'
-export const store = configureStore({
-    reducer:{
-        pathh: newSlice
-    }
-})
