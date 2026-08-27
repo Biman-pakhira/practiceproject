01f42fc4bbfee824806e5704d36ec8e481586c35
Biman Pakhira <bimanpakhira2@gmail.com>
2026-08-27 09:24:41 +0530

    now created

--- interview/server.js
+++ interview/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
-
+const path=require('path')
 const app = express();
-app.listen(3000,(req,res,err)=>{
-    if(err) console.error(err);
-    
+app.listen(3000,()=>{
     console.log("App is running on port 3000");
+})
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"in.html"),(err)=>{
+        if(err) console.error(err);
+        
+    })
 })
